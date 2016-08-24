eacd47a6039add1d3c883c2fd6b3d2e335d6241b
MrIzual <devilskullhp@gmail.com>
2016-08-24 10:36:53 +0700

    Create Main.php

---
+++ Main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Example;
+
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extend PluginBase implements Listener {
+
+        public function onEnabled( ) {
+                $this->getServer( )->getPluginManager( )->registerEvents($this, $this);
+                $this->getLogger( )->info(TextFormat::GREEN . ("Example Enabled.");
+        }
+        
+        public funtion onDisable( ) {
+                $this->getLogger( )->info(TextFormat::RED . ("Example Disabled.")
+        }
+}
