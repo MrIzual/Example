5ee006aa910a3185914937f42d017c7c17e020cf
MrIzual <devilskullhp@gmail.com>
2016-08-25 22:18:03 +0700

    Update Main.php

--- src/Example/Test/Main.php
+++ src/Example/Test/Main.php
@@ -4,19 +4,46 @@ namespace Example;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
-class Main extend PluginBase implements Listener {
-        public function onLoad( ) {
-                $this->getLogger->info(TextFormat::BLUE . "Example Loading.");
-                
+class Main extends PluginBase implements Listener {
+	
+	public function onEnable( ) {
+			    $this->getServer( )->getPluginManager( )->registerEvents($this, $this);
+				$this->saveDefaultConfig();
+		        $this->getLogger( )->info(TextFormat::GREEN . "[WindyRules]" .TextFormat::YELLOW. "Rules Enabled");
         }
-        
-        public function onEnable( ) {
-                $this->getServer( )->getPluginManager( )->registerEvents($this, $this);
-                $this->getLogger( )->info(TextFormat::GREEN . "Example Enabled.");
-        }
-        
-        public function onDisable( ){
-                $this->getLogger( )->info(TextFormat::RED . "Example Disabled.")
-        }
-}
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        $cmd = strtolower($command->getName());
+        switch ($cmd){
+            case "rules":
+                if (!($sender instanceof Player)){
+                    $sender->sendMessage(TEXTFORMAT::GOLD . "--------[xWindyRulesx]--------");
+                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule1"));
+                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule2"));
+                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule3"));
+                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule4"));
+                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule5"));
+                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule6"));
+                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule7"));
+                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule8"));
+                    return true;
+                }
+                $player = $this->getServer()->getPlayer($sender->getName());
+                if ($player->hasPermission("windyrules.rules")){
+                    $sender->sendMessage("--------[xWindyRulesx]--------");
+                    $sender->sendMessage("- " . $this->getConfig()->get("rule1"));
+                    $sender->sendMessage("- " . $this->getConfig()->get("rule2"));
+                    $sender->sendMessage("- " . $this->getConfig()->get("rule3"));
+                    $sender->sendMessage("- " . $this->getConfig()->get("rule4"));
+                    $sender->sendMessage("- " . $this->getConfig()->get("rule5"));
+                    $sender->sendMessage("- " . $this->getConfig()->get("rule6"));
+                    $sender->sendMessage("- " . $this->getConfig()->get("rule7"));
+                    $sender->sendMessage("- " . $this->getConfig()->get("rule8"));
+				}
+                    return true;
+			}
+	    }
+    }	  		
+?>
