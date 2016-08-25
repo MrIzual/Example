958cc019406f2386b5cbd4b870045dcdad395964
MrIzual <devilskullhp@gmail.com>
2016-08-25 19:46:51 +0700

    Update Main.php

--- src/Example/Test/Main.php
+++ src/Example/Test/Main.php
@@ -5,7 +5,8 @@ use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 
-class Main extend PluginBase implements Listene {
+class Main extend PluginBase implements Listener {
+        
         public function onEnable( ) {
                 $this->getServer( )->getPluginManager( )->registerEvents($this, $this);
                 $this->getLogger( )->info(TextFormat::GREEN . "Example Enabled.");
