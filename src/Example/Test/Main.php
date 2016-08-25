5c940ff2d5ff7b50578654279122e7c106d07e21
MrIzual <devilskullhp@gmail.com>
2016-08-25 20:37:20 +0700

    Update Main.php

--- src/Example/Test/Main.php
+++ src/Example/Test/Main.php
@@ -6,6 +6,10 @@ use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
 
 class Main extend PluginBase implements Listener {
+        public function onLoad( ) {
+                $this->getLogger->info(TextFormat::BLUE . "Example Loading.");
+                
+        }
         
         public function onEnable( ) {
                 $this->getServer( )->getPluginManager( )->registerEvents($this, $this);
